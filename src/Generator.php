e3cfccdbec8d7f2046097735b2d85b0fd2ee15f6
ZsgsDesign <zsgsdesign@gmail.com>
2019-06-30 17:18:12 +0800

    modify Generator

--- src/Generator.php
+++ src/Generator.php
@@ -4,13 +4,6 @@ namespace POEM;
 
 class Generator
 {
-    private $poemRaw="{}";
-
-    public function poemRaw()
-    {
-        return $this->poemRaw;
-    }
-
     private function isJson($string)
     {
         return ((is_string($string) &&
@@ -18,24 +11,14 @@ class Generator
                 is_array(json_decode($string))))) ? true : false;
     }
 
-    public function generate($poemRaw, $type="poem")
+    public function generate($data, $type="poem")
     {
-        $this->poemRaw=$poemRaw;
-        if ($type=="auto") {
-            $type=$this->isJson($this->poemRaw)?"poem":"poetry";
-        }
-
         if ($type=="poem") {
-            $ret=json_decode($poemRaw, true);
-            if(!is_array($ret)) $ret=[];
+            $ret=json_encode($data);
+            if(!isJson($ret)) $ret="{}";
         } elseif ($type=="poetry") {
             //unzip it
         }
-
-        foreach($ret["problems"] as $prob) {
-            $requirementProc($prob["require"]);
-        }
-
         return $ret;
     }
 }
