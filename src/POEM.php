1c20c46ad6262ba18366295102f3d006d6421f81
ZsgsDesign <zsgsdesign@gmail.com>
2019-10-10 14:47:49 +0800

    separate POEM Format

---
+++ src/POEM.php
@@ -0,0 +1,160 @@
+<?php
+ 
+namespace POEM;
+
+use Exception;
+use \PhpZip\ZipFile;
+ 
+class POEM
+{
+    private $workspace=null;
+    private $rawJSON=null;
+    private $data=[
+        "standard"=> "1.0",
+        "generator"=> [
+            "creator"=>[
+                "name"=> "",
+                "url"=> "",
+                "version"=> ""
+            ],
+            "producer"=> [
+                "name"=> "POEM_NPM",
+                "url"=> "https://github.com/NJUPTAAA/POEM_NPM",
+                "version"=> "1.1.0"
+            ]
+        ],
+        "title"=> "Untitled Problem",
+        "require"=> [],
+        "category"=> "OnlineJudge",
+        "resourcesFolder"=> "resources",
+        "timeLimit"=> 1000,
+        "memoryLimit"=> 262144,
+        "description"=> "description.md",
+        "input"=> "input.md",
+        "output"=> "output.md",
+        "note"=> "note.md",
+        "source"=> [
+            "url"=> false,
+            "name"=> false
+        ],
+        "sample"=> [
+            "input"=> "1 2\n",
+            "output"=> "3\n",
+            "note"=> null
+        ],
+        "extra"=> [
+            "markdown"=> true,
+            "forceRaw"=> false,
+            "partial"=> true,
+            "totScore"=> 3
+        ],
+        "testCasesFolder"=> "testcases",
+        "specialJudge"=> false,
+        "solutions"=> [
+            "locde"=> "cpp",
+            "source"=> "stds/std.cpp"
+        ]
+    ];
+
+    public function __toString()
+    {
+        return $this->rawJSON;
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined property via __get(): ' . $name .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+        return null;
+    }
+
+    public function __put($name)
+    {
+
+    }
+
+    public function __construct($workspace=null)
+    {
+        if(is_null($workspace)){
+            $this->workspace=Utils::createTmpFolder();
+            mkdir($this->workspace.DIRECTORY_SEPARATOR.'resources', 0700, true);
+            mkdir($this->workspace.DIRECTORY_SEPARATOR.'spj', 0700, true);
+            mkdir($this->workspace.DIRECTORY_SEPARATOR.'stds', 0700, true);
+            mkdir($this->workspace.DIRECTORY_SEPARATOR.'testcases', 0700, true);
+        }
+        $this->workspace=$workspace;
+    }
+
+    public function importJSON($json)
+    {
+        $this->rawJSON=$json;
+        $this->data=json_decode($json);
+        return $this;
+    }
+
+    
+    public function getProblemList()
+    {
+        if($this->type!="poetry") throw new Exception("Unsupported Method");
+        $list=json_decode(Utils::getFile("$this->path/main.json"),true);
+        $problems=$list["problems"];
+        $list["problems"]=[];
+        foreach($problems as $problem){
+            $list["problems"][]=(new Parser())->parseFile("$this->path/$problem", "poem");
+        }
+        return $list;
+    }
+
+    public function getProblemDetails()
+    {
+        if($this->type!="poem") throw new Exception("Unsupported Method");
+        if(is_null($this->rawJSON)){
+            $this->rawJSON=json_decode(Utils::getFile("$this->path/main.json"), true);
+        }
+        return $this->rawJSON;
+    }
+
+    public function getTestCasesZip()
+    {
+        if($this->type!="poem") throw new Exception("Unsupported Method");
+        // returns ZipFile instance
+        return (new ZipFile())->addDir($this->path.DIRECTORY_SEPARATOR.$this->getProblemDetails()['testCasesFolder']);
+    }
+
+    public function getSTDs()
+    {
+        if($this->type!="poem") throw new Exception("Unsupported Method");
+        if(is_null($this->solution)){
+            $this->solution=$this->getProblemDetails()['solution'];
+            foreach($this->solution as &$solution){
+                $solution['source']=Utils::getFile($this->path.DIRECTORY_SEPARATOR.$solution['source']);
+            }
+        }
+        return $this->solution;
+    }
+
+    public function getSPJ()
+    {
+        if($this->type!="poem") throw new Exception("Unsupported Method");
+        // get problem spj
+    }
+
+    public function getResource()
+    {
+        if($this->type!="poem") throw new Exception("Unsupported Method");
+        // get problem resource
+    }
+
+    public function getResources()
+    {
+        if($this->type!="poem") throw new Exception("Unsupported Method");
+        // get problem resources
+    }
+}
--- src/Parser.php
+++ src/Parser.php
@@ -11,106 +11,26 @@ class Parser
     private $path=null;
     private $type=null;
 
-    private function isJson($string)
-    {
-        return ((is_string($string) &&
-                (is_object(json_decode($string)) ||
-                is_array(json_decode($string))))) ? true : false;
-    }
-
-    private function generateRandomString($length = 10) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
-
-    private function createTmpFolder($tries=5)
-    {
-        $tmpFolder=__DIR__."/tmp/NOJ".time().$this->generateRandomString(6);
-        if(is_dir($tmpFolder)) return createTmpFolder(--$tries);
-        mkdir($tmpFolder, 0700, true);
-        return $tmpFolder;
-    }
-
-    private function getFile($path)
-    {
-        $file=file_get_contents($path);
-        if($file===false) throw new Exception("$path: File Not Found");
-        return $file;
-    }
-
     public function parseFile($poemFile, $type)
     {
-        return $this->parseStream($this->getFile($poemFile), $type);
+        return $this->parseStream(Utils::getFile($poemFile), $type);
     }
 
     public function parseStream($poemStream, $type)
     {
         if(!in_array($type,['poetry','poem'])) throw new Exception("Unsupported Type"); 
         $this->type=$type;
-        $tmpFolder=$this->createTmpFolder();
+        $tmpFolder=Utils::createTmpFolder();
         $zipFile = new ZipFile();
         $zipFile->openFromString($poemStream)->extractTo($tmpFolder);
         $this->path=$tmpFolder;
-        if(!$this->isJson($this->getFile("$this->path/main.json"))) throw new Exception("Malformed Files");
-
-        return $this;
-    }
-
-    public function getProblemList()
-    {
-        if($this->type!="poetry") throw new Exception("Unsupported Method");
-        $list=json_decode($this->getFile("$this->path/main.json"),true);
-        $problems=$list["problems"];
-        $list["problems"]=[];
-        foreach($problems as $problem){
-            $list["problems"][]=(new Parser())->parseFile("$this->path/$problem", "poem");
-        }
-        return $list;
-    }
-
-    public function getProblemDetails()
-    {
-        if($this->type!="poem") throw new Exception("Unsupported Method");
-        return json_decode($this->getFile("$this->path/main.json"), true);
-    }
-
-    public function getTestcases()
-    {
-        if($this->type!="poem") throw new Exception("Unsupported Method");
-        // get problem testcases
-    }
-
-    public function getSTDs()
-    {
-        if($this->type!="poem") throw new Exception("Unsupported Method");
-        // get problem stds
-    }
-
-    public function getSPJ()
-    {
-        if($this->type!="poem") throw new Exception("Unsupported Method");
-        // get problem spj
-    }
-
-    public function getResource()
-    {
-        if($this->type!="poem") throw new Exception("Unsupported Method");
-        // get problem resource
-    }
-
-    public function getResources()
-    {
-        if($this->type!="poem") throw new Exception("Unsupported Method");
-        // get problem resources
+        $json=Utils::getFile("$this->path/main.json");
+        if(!Utils::isJson($json)) throw new Exception("Malformed Files");
+        return (new POEM($this->path))->importJSON($json);
     }
 
     public function terminate()
     {
-
+        Utils::removeDir($this->path);
     }
 }
