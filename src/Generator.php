603bf6f33b8b52786aa9d4f3823677a97a34af3c
ZsgsDesign <zsgsdesign@gmail.com>
2019-10-10 15:45:04 +0800

    fixed generator

--- src/Generator.php
+++ src/Generator.php
@@ -4,18 +4,12 @@ namespace POEM;
 
 class Generator
 {
-    private function isJson($string)
-    {
-        return ((is_string($string) &&
-                (is_object(json_decode($string)) ||
-                is_array(json_decode($string))))) ? true : false;
-    }
 
     public function generate($data, $type="poem")
     {
         if ($type=="poem") {
             $ret=json_encode($data);
-            if(!isJson($ret)) $ret="{}";
+            if(!Utils::isJson($ret)) $ret="{}";
         } elseif ($type=="poetry") {
             //unzip it
         }
